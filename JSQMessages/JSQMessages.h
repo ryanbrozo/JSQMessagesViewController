c8e705256427c25a24bcc7d6394c41fcd56aadc8
Ryan Brozo <ryanbrozo@gmail.com>
2016-08-18 08:45:29 +0800

    initial import

---
+++ JSQMessages/JSQMessages.h
@@ -0,0 +1,19 @@
+//
+//  JSQMessages.h
+//  JSQMessages
+//
+//  Created by Ryan Brozo on 18/08/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSQMessages.
+FOUNDATION_EXPORT double JSQMessagesVersionNumber;
+
+//! Project version string for JSQMessages.
+FOUNDATION_EXPORT const unsigned char JSQMessagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSQMessages/PublicHeader.h>
+
+
